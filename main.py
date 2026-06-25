8ead120a202f43176b62122fd91a0697e9d2537e
giant-t <giant.takajin@gmail.com>
2026-06-25 17:18:11 +0900

    調整

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from streamlit-app-deploy!")
+import streamlit as st
 
+st.title("サンプルアプリ①: 簡単なWebアプリ")
 
-if __name__ == "__main__":
-    main()
+input_message = st.text_input(label="文字数のカウント対象となるテキストを入力してください。")
+
+text_count = len(input_message)
+
+if st.button("実行"):
+
+    st.write(f"文字数: **{text_count}**")
+    
